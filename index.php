4160f5ce247ba201ebd879c877ebfcf0f5328ed5
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-08-30 03:24:13 +0800

    8/30
    can echo result

--- index.php
+++ index.php
@@ -1,37 +1,46 @@
 <?php
   include_once "includes/dbh.inc.php";
-  
+   
 
   //check if is post
-  if(isset($_POST['userName']) &&isset( $_POST['passWord'])&& isset($_POST['eMail'])&& isset($_POST['birthDay'])){
+  if(isset($_POST['userName']) &&isset($_POST['passWord'])){
     $userName = $_POST['userName'];
     $passWord = $_POST['passWord'];
-    //$eMail =  $_POST['eMail'];
-    //$birthDay =$_POST['birthDay'];
     $response = array();
   }
 
 
 
-  $query = "select * from usert where userName = ? and passWord = ?";
+  $query = "select UID, userName, passWord, email from usert where userName = ? and passWord = ?";
 
   $stmt = mysqli_stmt_init($conn);
 
   if(!mysqli_stmt_prepare($stmt, $query)){
     echo "err!!! prepare wrong";
+    $response['result'] = 0;
+    $response['errMsg'] = "err!!! prepare wrong!!! please contect offical";
+    echo json_encode($response);
   }else {
     mysqli_stmt_bind_param($stmt,"ss",$userName, $passWord);
     mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_store_result($stmt);
+    mysqli_stmt_bind_result($stmt, $UID, $userName, $passWord, $eMail);
+    mysqli_stmt_store_result($stmt);
     
     if(mysqli_stmt_num_rows($stmt) ){
       while(mysqli_stmt_fetch($stmt)){
-        printf("%s, %s", $userName,$passWord);
-      }      
-      echo "has value";
+        $response['result']=1;
+        $response['successMsg']="Login success";
+        $response['UID']=$UID;
+        $response['userName']=$userName;
+        $response['passWord']=$passWord;
+        $response['eMail']=$eMail;
+
+        echo json_encode($response);
+      } 
     }else {
-      echo "NO value";
+      $response['result'] = 0;
+      $response['errMsg'] = "找不到相關帳號，請確認帳密是否有誤";
+      echo json_encode($response);
     }
-    echo "finish";
   }
  ?>
--- temp.php
+++ temp.php
@@ -1,10 +1,26 @@
 <?php
-    include_once "dbClass.php";
-    $arr = array(
-        "foo" => "bar",
-        "bar" => "foo",
-    );
+  include_once "includes/dbh.inc.php";
+    $pass = "Justin";
+    $query = "select UID, userName from usert where passWord = ?";
+    $stmt = mysqli_stmt_init($conn);
 
-    $test = new dbFunc($arr);
-    $test->printer();
-?>
+    if(!mysqli_stmt_prepare($stmt, $query)){
+        echo "err!!! prepare wrong";
+    }else {
+        mysqli_stmt_bind_param($stmt,"s", $pass);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $UID, $userName);
+        mysqli_stmt_store_result($stmt);
+        if(mysqli_stmt_num_rows($stmt)>0){
+            while(mysqli_stmt_fetch($stmt)){
+                $id = $UID;
+                $NAM = $userName;
+                printf ("%s (%s)\n", $NAM, $id);
+                echo "\nhas value";
+            }
+        }else{
+            echo "no value";
+        }
+    }
+    mysqli_close($conn);
+ ?>
