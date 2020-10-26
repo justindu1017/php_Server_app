d461b4ab71ddca2823675ed1f945b99d66ace960
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-10-26 08:26:28 +0800

    cant find ArticalContent, check URL

--- articalContent.php
+++ articalContent.php
@@ -1,6 +1,46 @@
+<?php
+  include_once "includes/dbh.inc.php";
+  $response = array();
+  if(isset($_POST['AID'])){
+    $AID = $_POST['AID'];
+  }else{
+    $response["result"] = 0;
+    $response["ErrMsg"] = "err!!! Post Method wrong!!! please contect official";
+    echo json_encode($response);
+    mysqli_close($conn);
+  }
 
- <?php
-  $response['result'] = 0;
-  $response['ErrMsg'] = "err!!! prepare wrong!!! please contect offical";
-  echo json_encode($response);
-?>
+
+
+  // $query = "select articalContent from T_artical where AID = ?";
+  $query = "select articalContent from T_artical where AID = 1000000";
+  $stmt = mysqli_stmt_init($conn);
+
+  if(!mysqli_stmt_prepare($stmt, $query)){
+    echo "err!!! prepare wrong";
+    $response['result'] = 0;
+    $response['ErrMsg'] = "err!!! prepare wrong!!! please contect offical";
+    echo json_encode($response);
+    mysqli_close($conn);
+  }else {
+    // mysqli_stmt_bind_param($stmt,"s",$articalContent);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $articalContent);
+    mysqli_stmt_store_result($stmt);
+    
+    if(mysqli_stmt_num_rows($stmt) ){
+      while(mysqli_stmt_fetch($stmt)){
+        $response['result']=1;
+        $response['articalContent']=$articalContent;
+
+        echo json_encode($response);
+        mysqli_close($conn);
+      } 
+    }else {
+      $response['result'] = 0;
+      $response['ErrMsg'] = "找不到文章";
+      echo json_encode($response);
+      mysqli_close($conn);
+    }
+  }
+ ?>
--- index.php
+++ index.php
@@ -1,100 +1,51 @@
+<?php
+  include_once "includes/dbh.inc.php";
+  $response = array();
+  if(isset($_POST['userName']) &&isset($_POST['passWord'])){
+    $userName = $_POST['userName'];
+    $passWord = $_POST['passWord'];
+  }else{
+    $response["result"] = 0;
+    $response["ErrMsg"] = "err!!! Post Method wrong!!! please contect official";
+    echo json_encode($response);
+    mysqli_close($conn);
+  }
 
- <?php
-    include_once "includes/dbh.inc.php";
 
-    $response = array();
 
-    if(isset($_POST["type"])){
+  $query = "select UID, userName, passWord, email from usert where userName = ? and passWord = ?";
 
-      $type = $_POST["type"];
-      if($type == "Loadform"){
+  $stmt = mysqli_stmt_init($conn);
 
-        $startPoint = $_POST["startPoint"];
+  if(!mysqli_stmt_prepare($stmt, $query)){
+    echo "err!!! prepare wrong";
+    $response['result'] = 0;
+    $response['ErrMsg'] = "err!!! prepare wrong!!! please contect offical";
+    echo json_encode($response);
+    mysqli_close($conn);
+  }else {
+    mysqli_stmt_bind_param($stmt,"ss",$userName, $passWord);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $UID, $userName, $passWord, $eMail);
+    mysqli_stmt_store_result($stmt);
+    
+    if(mysqli_stmt_num_rows($stmt) ){
+      while(mysqli_stmt_fetch($stmt)){
+        $response['result']=1;
+        $response['successMsg']="Login success";
+        $response['UID']=$UID;
+        $response['userName']=$userName;
+        $response['passWord']=$passWord;
+        $response['eMail']=$eMail;
 
-        $query = "Select AID, userName, articalTitle, CDate from T_artical ORDER BY CDate Limit 10 OFFSET ? ";
-
-        $stmt  = mysqli_stmt_init($conn);
-
-        if(!mysqli_stmt_prepare($stmt, $query)){
-            echo "err!!! prepare wrong!!!!!!!";
-            $response['result'] = 0;
-            $response['ErrMsg'] = "err!!! prepare wrong!!! please contect offical";
-            echo json_encode($response);
-            mysqli_close($conn);
-          }else {
-            mysqli_stmt_bind_param($stmt,"i", $startPoint);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_bind_result($stmt, $AID, $userName, $articalTitle,$CDate);
-            mysqli_stmt_store_result($stmt);
-
-            if(mysqli_stmt_error($stmt)){
-                $response['result'] = 0;
-                $response['ErrMsg'] = mysqli_stmt_error($stmt);
-                echo json_encode($response);
-                mysqli_close($conn);
-
-            }else{
-                $response['result'] = 1;
-                $reArray = array();
-                while(mysqli_stmt_fetch($stmt)){
-                  $tempArray = array();
-                  array_push($tempArray, $AID);
-                  array_push($tempArray, $userName);
-                  array_push($tempArray, $articalTitle);
-                  array_push($tempArray, $CDate);
-                  array_push($reArray, $tempArray);
-                }
-                $response['Arr'] = $reArray;
-                echo json_encode($response);
-                mysqli_close($conn);
-            }
-          }
-
-      }
-    elseif($type == "getContent"){
-        $AID = $_POST["AID"];
-
-        $query = "Select articalContent  from T_artical where  AID = ?";
-
-        $stmt  = mysqli_stmt_init($conn);
-
-        if(!mysqli_stmt_prepare($stmt, $query)){
-            echo "err!!! prepare wrong!!!!!!!";
-            $response['result'] = 0;
-            $response['ErrMsg'] = "err!!! prepare wrong!!! please contect offical";
-            echo json_encode($response);
-            mysqli_close($conn);
-          }else {
-            mysqli_stmt_bind_param($stmt,"i", $AID);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_bind_result($stmt, $articalContent);
-            mysqli_stmt_store_result($stmt);
-
-            if(mysqli_stmt_error($stmt)){
-                $response['result'] = 0;
-                $response['ErrMsg'] = mysqli_stmt_error($stmt);
-                echo json_encode($response);
-                mysqli_close($conn);
-
-            }else{
-                $response['result'] = 1;
-                if(mysqli_stmt_num_rows($stmt) ){
-                  while(mysqli_stmt_fetch($stmt)){
-                    $response['articalContent'] = $articalContent;
-                  }
-                }
-                echo json_encode($response);
-                mysqli_close($conn);
-            }
-          }
-        }
-    }
-
-    else{
-        $response["result"] = 0;
-        $response["ErrMsg"] = "err!!! Post Method wrong!!! please contect official";
         echo json_encode($response);
         mysqli_close($conn);
-      }
-
-?>
+      } 
+    }else {
+      $response['result'] = 0;
+      $response['ErrMsg'] = "找不到帳號，請確認帳密是否有誤";
+      echo json_encode($response);
+      mysqli_close($conn);
+    }
+  }
+ ?>
