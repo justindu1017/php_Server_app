237cb5365414a8ab321bc63e42947c3e6c2b0432
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-11-02 03:03:09 +0800

    Remote

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,15 @@
 <?php
-  $dbLocation = "localhost";
-  $userName = "root";
-  $userPass = "";
-  $dbname = "EE_Project";
+  // $dbLocation = "localhost";
+  // $userName = "root";
+  // $userPass = "";
+  // $dbname = "EE_Project";
+
+
+  $dbLocation = "remotemysql.com";
+  $userName = "mI0wmLk8iA";
+  $userPass = "LAi31XTuZW";
+  $dbname = "mI0wmLk8iA";
+
 
   $conn = mysqli_connect($dbLocation, $userName, $userPass, $dbname);
  ?>
