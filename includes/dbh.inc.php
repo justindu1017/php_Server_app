6ab1f714a1a479c84823f0cb6c8e71cb04f6a338
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-11-14 02:10:37 +0800

    testing ArticalContest

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,14 +1,14 @@
 <?php
-  // $dbLocation = "localhost";
-  // $userName = "root";
-  // $userPass = "";
-  // $dbname = "EE_Project";
+  $dbLocation = "localhost";
+  $userName = "root";
+  $userPass = "";
+  $dbname = "EE_Project";
 
 
-  $dbLocation = "remotemysql.com";
-  $userName = "mI0wmLk8iA";
-  $userPass = "LAi31XTuZW";
-  $dbname = "mI0wmLk8iA";
+  // $dbLocation = "remotemysql.com";
+  // $userName = "mI0wmLk8iA";
+  // $userPass = "LAi31XTuZW";
+  // $dbname = "mI0wmLk8iA";
 
 
   $conn = mysqli_connect($dbLocation, $userName, $userPass, $dbname);
