edfbad056778b6f4822659587192905a97fba90a
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-11-23 03:30:49 +0800

    ee

--- temp.php
+++ temp.php
@@ -3,7 +3,7 @@
 
     // $hash = password_hash($String,PASSWORD_DEFAULT);
     // echo $hash;
-    // echo "                                \\\\                          "  ;
-    echo password_verify(1,"$2y$10\$b3Q9637Vz135dLYZFOSDY.I6k7f8y78kR2LOben7sn986btJocjx.");
+    echo "$2y$10\$VZNhPW1glbpWcjgfdwwn\/O4E2TXNi\/3W.u8xB.8F1.zuQA0R9MegS";
+    echo password_verify("1","$2y$10\$VZNhPW1glbpWcjgfdwwn\/O4E2TXNi\/3W.u8xB.8F1.zuQA0R9MegS");
     
 ?>
