cc22240e7905d92fcb1ea8ba97c163617058cb29
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-11-23 02:40:29 +0800

    testing login

--- index copy.php
+++ index copy.php
@@ -3,7 +3,7 @@
   $response = array();
   if(isset($_POST['userName']) &&isset($_POST['passWord'])){
     $userName = $_POST['userName'];
-    $passWordP = $_POST['passWord'];
+    $passWord = $_POST['passWord'];
   }else{
     $response["result"] = 0;
     $response["ErrMsg"] = "err!!! Post Method wrong!!! please contect official";
@@ -14,7 +14,7 @@
 
 
   // $query = "select UID, userName, passWord, email from usert where userName = ? and passWord = ?";
-  $query = "select UID, userName, passWord, email from usert where userName = ?";
+  $query = "select UID, userName, email from usert where userName = ? and passWord = ?";
   $stmt = mysqli_stmt_init($conn);
 
   if(!mysqli_stmt_prepare($stmt, $query)){
@@ -24,31 +24,24 @@
     echo json_encode($response);
     mysqli_close($conn);
   }else {
-    // $hash = password_hash($passWord, PASSWORD_DEFAULT);
-    // mysqli_stmt_bind_param($stmt,"ss",$userName, $hash);
-    mysqli_stmt_bind_param($stmt,"ss",$userName, $passWordP);
+    $hash = password_hash($passWord, PASSWORD_DEFAULT);
+    mysqli_stmt_bind_param($stmt,"ss",$userName, $hash);
+    // mysqli_stmt_bind_param($stmt,"ss",$userName, $passWord);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_bind_result($stmt, $UID, $userName, $passWord, $eMail);
     mysqli_stmt_store_result($stmt);
     
     if(mysqli_stmt_num_rows($stmt) ){
       while(mysqli_stmt_fetch($stmt)){
-        if(password_verify($passWordP,$passWord)){
-                $response['result']=1;
-                $response['successMsg']="Login success";
-                $response['UID']=$UID;
-                $response['userName']=$userName;
-                // $response['passWord']=$passWord;
-                $response['eMail']=$eMail;
+        $response['result']=1;
+        $response['successMsg']="Login success";
+        $response['UID']=$UID;
+        $response['userName']=$userName;
+        // $response['passWord']=$passWord;
+        $response['eMail']=$eMail;
 
-                echo json_encode($response);
-                mysqli_close($conn);
-        }else {
-          $response['result'] = 0;
-          $response['ErrMsg'] = "找不到帳號，請確認帳密是否有誤";
-          echo json_encode($response);
-          mysqli_close($conn);
-        }
+        echo json_encode($response);
+        mysqli_close($conn);
       } 
     }else {
       $response['result'] = 0;
@@ -57,3 +50,4 @@
       mysqli_close($conn);
     }
   }
+ ?>
